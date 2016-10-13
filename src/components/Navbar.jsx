337dab04b8a5905f829490943e45461645ed81f4
Daniel Yim <danielyim@gmail.com>
2016-10-13 17:35:31 +0700

    turned the import deck component into a modal built into the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { Link, browserHistory } from 'react-router';
+import ImportDeck from './ImportDeck';
 import styles from '../styles/components/Navbar';
 
 const propTypes = {};
@@ -7,6 +9,24 @@ const propTypes = {};
 const defaultProps = {};
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.openImportDeckModal = this.openImportDeckModal.bind(this);
+    this.closeImportDeckModal = this.closeImportDeckModal.bind(this);
+    this.routeParser = this.routeParser.bind(this);
+
+    this.state = { isImportDeckModalOpen: false };
+  }
+
+  openImportDeckModal() {
+    this.setState({ isImportDeckModalOpen: true });
+  }
+
+  closeImportDeckModal() {
+    this.setState({ isImportDeckModalOpen: false });
+  }
+
   routeParser(path) {
     let addLink;
     let listLink;
@@ -14,88 +34,33 @@ class Navbar extends React.Component {
 
     if (path === '/') {
       // @/
-      addLink = (
-        <Link to="/add" title="Add deck">
-          <i className="fa fa-plus-square-o"></i>
-        </Link>
-      );
+      // addLink = this.renderLink('/add', 'Add deck', 'fa-plus-square-o');
+      addLink = this.renderModal(this.openImportDeckModal, 'fa-plus-square-o');
       // listLink = null;
-      listLink = (
-        <Link to={`/settings`} title="Settings">
-          <i className="fa fa-cog"></i>
-        </Link>
-        // <a href="javascript:void(0);" disabled><i className="fa fa-navicon"></i></a>
-      );
-    }
-    else if (routeComponents[0] === 'view' && routeComponents.length === 2) {
+      listLink = this.renderLink('/settings', 'Settings', 'fa-cog');
+      // <a href="javascript:void(0);" disabled><i className="fa fa-navicon"></i></a>
+    } else if (routeComponents[0] === 'view' && routeComponents.length === 2) {
       // @/view/:deckId
-      addLink = (
-        <Link to={`/add/${routeComponents[1]}`} title="Add card">
-          <i className="fa fa-plus-square-o"></i>
-        </Link>
-      );
-      listLink = (
-        <Link to={`/`} title="View decks">
-          <i className="fa fa-clone"></i>
-        </Link>
-      );
-    }
-    else if (routeComponents[0] === 'add' && routeComponents.length === 2) {
+      addLink = this.renderLink(`/add/${routeComponents[1]}`, 'Add card', 'fa-plus-square-o');
+      listLink = this.renderLink('/', 'View decks', 'fa-clone');
+    } else if (routeComponents[0] === 'add' && routeComponents.length === 2) {
       // @/add/:deckId
-      addLink = (
-        <Link to={`/add/${routeComponents[1]}`} title="Add card">
-          <i className="fa fa-plus-square-o"></i>
-        </Link>
-      );
-      listLink = (
-        <Link to={`/view/${routeComponents[1]}`} title="View cards">
-          <i className="fa fa-square-o"></i>
-        </Link>
-      );
-    }
-    else if (routeComponents[0] === 'view' && routeComponents.length === 3) {
+      addLink = this.renderLink(`/add/${routeComponents[1]}`, 'Add card', 'fa-plus-square-o');
+      listLink = this.renderLink(`/view/${routeComponents[1]}`, 'View cards', 'fa-square-o');
+    } else if (routeComponents[0] === 'view' && routeComponents.length === 3) {
       // @/view/:deckId/:cardId
-      addLink = (
-        <Link to={`/add/${routeComponents[1]}`} title="Add card">
-          <i className="fa fa-plus-square-o"></i>
-        </Link>
-      );
-      listLink = (
-        <Link to={`/view/${routeComponents[1]}`} title="View cards">
-          <i className="fa fa-square-o"></i>
-        </Link>
-      );
-    }
-    else if (routeComponents[0] === 'add' && routeComponents.length === 1) {
+      addLink = this.renderLink(`/add/${routeComponents[1]}`, 'Add card', 'fa-plus-square-o');
+      listLink = this.renderLink(`/view/${routeComponents[1]}`, 'View cards', 'fa-square-o');
+    } else if (routeComponents[0] === 'add' && routeComponents.length === 1) {
       // @/add
-      addLink = (
-        <a href="javascript:void(0);" disabled>
-          <i className="fa fa-plus-square-o"></i>
-        </a>
-      );
-      listLink = (
-        <Link to={'/'} title="View decks"><i className="fa fa-th"></i></Link>
-      );
-    }
-    else if(routeComponents[0] === 'edit' && routeComponents.length === 3) {
+      addLink = this.renderNoAction();
+      listLink = this.renderLink('/', 'View decks', 'fa-th');
+    } else if (routeComponents[0] === 'edit' && routeComponents.length === 3) {
       // @/edit/:deckId/:cardIndex
-      addLink = (
-        <a href="javascript:void(0);" disabled>
-          <i className="fa fa-plus-square-o"></i>
-        </a>
-      );
-      listLink = (
-        <Link to={`/view/${routeComponents[1]}`} title="View cards">
-          <i className="fa fa-square-o"></i>
-        </Link>
-      );
-    }
-    else {
-      addLink = (
-        <a href="javascript:void(0);" disabled>
-          <i className="fa fa-plus-square-o"></i>
-        </a>
-      );
+      addLink = this.renderNoAction();
+      listLink = this.renderLink(`/view/${routeComponents[1]}`, 'View cards', 'fa-square-o');
+    } else {
+      addLink = this.renderNoAction();
       listLink = null;
       // listLink = (
       //   <a href="javascript:void(0);" disabled><i className="fa fa-navicon"></i></a>
@@ -106,6 +71,42 @@ class Navbar extends React.Component {
       addLink,
       listLink
     };
+  }
+
+  renderImportDeckModal() {
+    return (
+      <Modal
+        isOpen={this.state.isImportDeckModalOpen}
+        onRequestClose={this.closeImportDeckModal}
+        className={`${styles['modal-import-deck']}`}
+      >
+        <ImportDeck handleCancel={this.closeImportDeckModal} />
+      </Modal>
+    );
+  }
+
+  renderLink(url, title, faClassName) {
+    return (
+      <Link to={url} title={title}>
+        <i className={`fa ${faClassName}`} />
+      </Link>
+    );
+  }
+
+  renderModal(openModalFn, faClassName) {
+    return (
+      <a onClick={openModalFn}>
+        <i className={`fa ${faClassName}`} />
+      </a>
+    );
+  }
+
+  renderNoAction(faClassName = 'fa-plus-square-o') {
+    return (
+      <a href="javascript:void(0);" disabled>
+        <i className={`fa ${faClassName}`} />
+      </a>
+    );
   }
 
   render() {
@@ -136,6 +137,7 @@ class Navbar extends React.Component {
                 {listLink}
               </div>
             </span>
+            {this.renderImportDeckModal()}
           </div>
         </div>
       </nav>
