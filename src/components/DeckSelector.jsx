a9975d2b33eccf8b33fed2dcc5362f9d3b4e2745
Daniel Yim <danielyim@gmail.com>
2016-10-17 14:15:09 +0700

    adding scaffolding for the deck selector idea

---
+++ src/components/DeckSelector.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from '../styles/components/DeckSelector';
+
+const propTypes = {
+  decks: React.PropTypes.array.isRequired,
+  excludedDeckIds: React.PropTypes.array,
+  maxSelected: React.PropTypes.number.isRequired,
+  minSelected: React.PropTypes.number.isRequired,
+  handleOnSelected: React.PropTypes.func.isRequired
+};
+
+const defaultProps = {
+  maxSelected: 1,
+  minSelected: 1
+};
+
+/**
+ * Should be a simple pop-up of all the decks available to choose from,
+ * optionally excluding (or greying out) a particular deck from the list.
+ *
+ * Input: decks to choose from, deck to exclude from selection, number of decks
+ *   to allow for selection via [min, max]
+ * Output: deck(s) selected from the list
+ */
+class DeckSelector extends React.Component {
+  render() {
+    const { decks } = this.props;
+
+    return (
+      <div>
+        <p>Yo, motherfucker, here are your decks:</p>
+        {decks.map(x =>
+          <div className={`${styles['deck-list-item']}`}>
+            deck Id: {x.deckId}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+DeckSelector.propTypes = propTypes;
+DeckSelector.defaultProps = defaultProps;
+
+export default DeckSelector;
+
