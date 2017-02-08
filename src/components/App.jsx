630a536183de1d13cba7ddc9995df9cd8d185e41
Daniel Yim <danielyim@gmail.com>
2017-02-07 23:20:29 -0800

    fixing lint errors file by file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,36 +2,32 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const propTypes = {
-  children: React.PropTypes.array.isRequired
+  children: React.PropTypes.object.isRequired
 };
 
 const defaultProps = {};
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <main className="main-container">
-          {/* React.cloneElement(this.props.children, this.props) */}
-          {this.props.children}
-        </main>
-        <footer>
-          <p>
-            dyim_2016 |
-            <a
-              href="https://github.com/danyim/indecks"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github" />
-            </a>
-          </p>
-        </footer>
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <div>
+    <Navbar />
+    <main className="main-container">
+      {/* React.cloneElement(this.props.children, this.props) */}
+      {props.children}
+    </main>
+    <footer>
+      <p>
+        dyim_2016 |
+        <a
+          href="https://github.com/danyim/indecks"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-github" />
+        </a>
+      </p>
+    </footer>
+  </div>
+);
 
 App.propTypes = propTypes;
 App.defaultProps = defaultProps;
