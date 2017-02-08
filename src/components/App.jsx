bc5eecdffd2a36db8291511881f66e3a90b70b54
Daniel Yim <danielyim@gmail.com>
2017-02-07 17:23:45 -0800

    fixing eslintrc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+
+const propTypes = {
+  children: React.PropTypes.array.isRequired
+};
+
+const defaultProps = {};
 
 class App extends React.Component {
   render() {
@@ -11,11 +17,23 @@ class App extends React.Component {
           {this.props.children}
         </main>
         <footer>
-          <p>dyim_2016 | <a href="https://github.com/danyim/indecks" target="_blank"><i className="fa fa-github"></i></a></p>
+          <p>
+            dyim_2016 |
+            <a
+              href="https://github.com/danyim/indecks"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-github" />
+            </a>
+          </p>
         </footer>
       </div>
-    )
+    );
   }
 }
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
 
 export default App;
