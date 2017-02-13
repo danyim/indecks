9a22586e8ae7b503e10c54a85d4afb2ca0854527
Daniel Yim <danielyim@gmail.com>
2017-02-13 01:28:31 -0800

    removing comments

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,8 +4,6 @@ import { routerReducer as routing } from 'react-router-redux';
 
 import config from './modules/config';
 import decks from './modules/decks';
-// import config from './config';
-// import decks from './decks';
 
 export default combineReducers({
   config,
