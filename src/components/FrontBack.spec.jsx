cafef035217d33bb7efed1f541bf010cfef400d3
Daniel Yim <danielyim@gmail.com>
2017-03-04 00:07:09 +0800

    finishing tests for FrontBack

--- src/components/FrontBack.spec.jsx
+++ src/components/FrontBack.spec.jsx
@@ -3,28 +3,69 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import FrontBack from './FrontBack';
 
-function setup() {
-  const props = {
-    handleFlip: jest.fn(),
-    flipped: true
-  };
+const defaultProps = {
+  handleFlip: jest.fn(),
+  flipped: false
+};
 
-  const enzymeWrapper = shallow(<FrontBack {...props} />)
+function setup(props = defaultProps) {
+  const wrapper = shallow(<FrontBack {...props} />)
 
   return {
     props,
-    enzymeWrapper
+    wrapper
   }
 }
 
-describe('components', () => {
+describe('Front Back', () => {
   describe('Header', () => {
     it('should render self and subcomponents', () => {
-      const { enzymeWrapper } = setup();
-      expect(enzymeWrapper.find('div').hasClass('frontBack')).toBe(true);
-      // expect(enzymeWrapper.find('a[tabIndex=0]').text()).toBe('FRONT');
-      // expect(enzymeWrapper.find('a[tabIndex=-1]').text()).toBe('BACK');
-      // expect(true).toBe(true);
+      const { wrapper } = setup();
+      expect(wrapper.find('div').hasClass('frontBack')).toBe(true);
+      expect(wrapper.find('a.front').text()).toBe('FRONT');
+      expect(wrapper.find('a.back').text()).toBe('BACK');
+    });
+
+    it('should apply the active class to "BACK" when flipped property is TRUE', () => {
+      const { wrapper } = setup(
+        {
+          ... defaultProps,
+          flipped: true
+        }
+      );
+      expect(wrapper.find('a.active').text()).toBe('BACK');
+    });
+
+    it('should apply the active class to "FRONT" when flipped property is FALSE', () => {
+      const { wrapper } = setup(
+        {
+          ... defaultProps,
+          flipped: false
+        }
+      );
+      expect(wrapper.find('a.active').text()).toBe('FRONT');
+    });
+
+    it('should call the handleFlip property when FRONT is clicked', () => {
+      const handler = jest.fn();
+      const { wrapper } = setup({
+        ...defaultProps,
+        handleFlip: handler
+      });
+
+      wrapper.find('a.front').simulate('click');
+      expect(handler.mock.calls.length).toBe(1);
+    });
+
+    it('should call the handleFlip property when BACK is clicked', () => {
+      const handler = jest.fn();
+      const { wrapper } = setup({
+        ...defaultProps,
+        handleFlip: handler
+      });
+
+      wrapper.find('a.back').simulate('click');
+      expect(handler.mock.calls.length).toBe(1);
     });
   });
 });
