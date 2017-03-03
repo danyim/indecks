5df5e0c2e9fa6431136fe21c90aaafd95010f7c9
Daniel Yim <danielyim@gmail.com>
2017-03-04 03:06:29 +0800

    updating CI node versions to node 7

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: [
-    // require('postcss-smart-import')({ /* ...options */ }),
-    // require('precss')({ /* ...options */ }),
     require('autoprefixer')({ browsers: ['last 2 versions'] })
   ]
 };
