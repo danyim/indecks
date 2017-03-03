3a819ff86465686ce2d9c4f0525344fa9e75fa9d
Daniel Yim <danielyim@gmail.com>
2017-03-04 02:59:06 +0800

    adding tests for CardCount

---
+++ src/components/CardCount.spec.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CardCount from './CardCount';
+
+const defaultProps = {
+  current: 25,
+  max: 100
+};
+
+function setup(props = defaultProps) {
+  const wrapper = shallow(<CardCount {...props} />)
+
+  return {
+    props,
+    wrapper
+  }
+}
+
+describe('CardCount', () => {
+  it('should render self and subcomponents', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find('div').hasClass('cardCount')).toBe(true);
+  });
+
+  it('should render the correct card current/max', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find('span.current').text()).toEqual(`${defaultProps.current}`);
+    expect(wrapper.find('span.max').text()).toEqual(`${defaultProps.max}`);
+  });
+});
--- src/components/FrontBack.spec.jsx
+++ src/components/FrontBack.spec.jsx
@@ -17,55 +17,53 @@ function setup(props = defaultProps) {
   }
 }
 
-describe('Front Back', () => {
-  describe('Header', () => {
-    it('should render self and subcomponents', () => {
-      const { wrapper } = setup();
-      expect(wrapper.find('div').hasClass('frontBack')).toBe(true);
-      expect(wrapper.find('a.front').text()).toBe('FRONT');
-      expect(wrapper.find('a.back').text()).toBe('BACK');
+describe('FrontBack', () => {
+  it('should render self and subcomponents', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find('div').hasClass('frontBack')).toBe(true);
+    expect(wrapper.find('a.front').text()).toBe('FRONT');
+    expect(wrapper.find('a.back').text()).toBe('BACK');
+  });
+
+  it('should apply the active class to "BACK" when flipped property is TRUE', () => {
+    const { wrapper } = setup(
+      {
+        ... defaultProps,
+        flipped: true
+      }
+    );
+    expect(wrapper.find('a.active').text()).toBe('BACK');
+  });
+
+  it('should apply the active class to "FRONT" when flipped property is FALSE', () => {
+    const { wrapper } = setup(
+      {
+        ... defaultProps,
+        flipped: false
+      }
+    );
+    expect(wrapper.find('a.active').text()).toBe('FRONT');
+  });
+
+  it('should call the handleFlip property when FRONT is clicked', () => {
+    const handler = jest.fn();
+    const { wrapper } = setup({
+      ...defaultProps,
+      handleFlip: handler
     });
 
-    it('should apply the active class to "BACK" when flipped property is TRUE', () => {
-      const { wrapper } = setup(
-        {
-          ... defaultProps,
-          flipped: true
-        }
-      );
-      expect(wrapper.find('a.active').text()).toBe('BACK');
+    wrapper.find('a.front').simulate('click');
+    expect(handler.mock.calls.length).toBe(1);
+  });
+
+  it('should call the handleFlip property when BACK is clicked', () => {
+    const handler = jest.fn();
+    const { wrapper } = setup({
+      ...defaultProps,
+      handleFlip: handler
     });
 
-    it('should apply the active class to "FRONT" when flipped property is FALSE', () => {
-      const { wrapper } = setup(
-        {
-          ... defaultProps,
-          flipped: false
-        }
-      );
-      expect(wrapper.find('a.active').text()).toBe('FRONT');
-    });
-
-    it('should call the handleFlip property when FRONT is clicked', () => {
-      const handler = jest.fn();
-      const { wrapper } = setup({
-        ...defaultProps,
-        handleFlip: handler
-      });
-
-      wrapper.find('a.front').simulate('click');
-      expect(handler.mock.calls.length).toBe(1);
-    });
-
-    it('should call the handleFlip property when BACK is clicked', () => {
-      const handler = jest.fn();
-      const { wrapper } = setup({
-        ...defaultProps,
-        handleFlip: handler
-      });
-
-      wrapper.find('a.back').simulate('click');
-      expect(handler.mock.calls.length).toBe(1);
-    });
+    wrapper.find('a.back').simulate('click');
+    expect(handler.mock.calls.length).toBe(1);
   });
 });
