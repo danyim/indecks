e83d1c711db70830f7de0ea92d8469e91b8be312
Daniel Yim <danielyim@gmail.com>
2017-06-23 12:11:45 -0700

    adding proptypes dep and updating file

--- src/components/CardEdit.jsx
+++ src/components/CardEdit.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
 import { browserHistory } from 'react-router'
 import KeyBinding from 'react-keybinding-component'
 import CardEditForm from './CardEditForm'
