16f6075dcb9d50ef12c7c309059d323dc1d02a90
Daniel Yim <danielyim@gmail.com>
2018-07-04 00:55:11 -0700

    updating scripts

--- scripts/build.js
+++ scripts/build.js
@@ -55,14 +55,12 @@ measureFileSizesBeforeBuild(paths.appBuild)
         console.log(chalk.yellow('Compiled with warnings.\n'))
         console.log(warnings.join('\n\n'))
         console.log(
-          `\nSearch for the ${chalk.underline(
-            chalk.yellow('keywords')
-          )} to learn more about each warning.`
+          '\nSearch for the ' +
+            chalk.underline(chalk.yellow('keywords')) +
+            ' to learn more about each warning.',
         )
         console.log(
-          `To ignore, add ${chalk.cyan(
-            '// eslint-disable-next-line'
-          )} to the line before.\n`
+          'To ignore, add ' + chalk.cyan('// eslint-disable-next-line') + ' to the line before.\n',
         )
       } else {
         console.log(chalk.green('Compiled successfully.\n'))
@@ -74,7 +72,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
         previousFileSizes,
         paths.appBuild,
         WARN_AFTER_BUNDLE_GZIP_SIZE,
-        WARN_AFTER_CHUNK_GZIP_SIZE
+        WARN_AFTER_CHUNK_GZIP_SIZE,
       )
       console.log()
 
@@ -82,26 +80,20 @@ measureFileSizesBeforeBuild(paths.appBuild)
       const publicUrl = paths.publicUrl
       const publicPath = config.output.publicPath
       const buildFolder = path.relative(process.cwd(), paths.appBuild)
-      printHostingInstructions(
-        appPackage,
-        publicUrl,
-        publicPath,
-        buildFolder,
-        useYarn
-      )
+      printHostingInstructions(appPackage, publicUrl, publicPath, buildFolder, useYarn)
     },
     err => {
       console.log(chalk.red('Failed to compile.\n'))
       printBuildError(err)
       process.exit(1)
-    }
+    },
   )
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
   console.log('Creating an optimized production build...')
 
-  const compiler = webpack(config)
+  let compiler = webpack(config)
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
       if (err) {
@@ -118,22 +110,21 @@ function build(previousFileSizes) {
       }
       if (
         process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
+        (typeof process.env.CI !== 'string' || process.env.CI.toLowerCase() !== 'false') &&
         messages.warnings.length
       ) {
         console.log(
           chalk.yellow(
             '\nTreating warnings as errors because process.env.CI = true.\n' +
-              'Most CI servers set it automatically.\n'
-          )
+              'Most CI servers set it automatically.\n',
+          ),
         )
         return reject(new Error(messages.warnings.join('\n\n')))
       }
       return resolve({
         stats,
         previousFileSizes,
-        warnings: messages.warnings
+        warnings: messages.warnings,
       })
     })
   })
@@ -142,6 +133,6 @@ function build(previousFileSizes) {
 function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
     dereference: true,
-    filter: file => file !== paths.appHtml
+    filter: file => file !== paths.appHtml,
   })
 }
--- scripts/start.js
+++ scripts/start.js
@@ -41,8 +41,21 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 9406
 const HOST = process.env.HOST || '0.0.0.0'
 
+if (process.env.HOST) {
+  console.log(
+    chalk.cyan(
+      `Attempting to bind to HOST environment variable: ${chalk.yellow(
+        chalk.bold(process.env.HOST),
+      )}`,
+    ),
+  )
+  console.log(`If this was unintentional, check that you haven't mistakenly set it in your shell.`)
+  console.log(`Learn more here: ${chalk.yellow('http://bit.ly/2mwWSwH')}`)
+  console.log()
+}
+
 // We attempt to use the default port but if it is busy, we offer the user to
-// run on a different port. `detect()` Promise resolves to the next free port.
+// run on a different port. `choosePort()` Promise resolves to the next free port.
 choosePort(HOST, DEFAULT_PORT)
   .then(port => {
     if (port == null) {
@@ -71,8 +84,8 @@ choosePort(HOST, DEFAULT_PORT)
       console.log(chalk.cyan('Starting the development server...\n'))
       openBrowser(urls.localUrlForBrowser)
     })
-    ;['SIGINT', 'SIGTERM'].forEach(sig => {
-      process.on(sig, () => {
+    ;['SIGINT', 'SIGTERM'].forEach(function(sig) {
+      process.on(sig, function() {
         devServer.close()
         process.exit()
       })
