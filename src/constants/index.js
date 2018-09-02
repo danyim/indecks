db6473a2492d27da884b116ef7322ffb82f1f0f3
Daniel Yim <danielyim@gmail.com>
2018-09-02 01:38:39 -0700

    adding a constants file

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+export const VIEW_STYLES = Object.freeze({
+  TWO_COL: 'TWO_COL',
+  THREE_COL: 'THREE_COL',
+  LIST: 'LIST',
+})
+
+export const SORT_TYPES = Object.freeze({
+  CREATED: 'CREATED',
+  EDITED: 'EDITED',
+  UNANSWERED: 'UNANSWERED',
+})
